935be8dca906a0a4f9f309f900c7f1b19a4fd0dc
dwctua <46290157+dwctua@users.noreply.github.com>
2020-05-03 12:09:41 -0400

    Update generate.groovy

--- maven/generate.groovy
+++ maven/generate.groovy
@@ -4,3 +4,13 @@ void call(){
     }
   
 }
+
+@BeforeStep
+void before(context){
+    println "generateSource: running before the ${context.library} library's ${context.step} step"
+}
+
+@AfterStep
+void after(context){
+    println "generateSource: running after the ${context.library} library's ${context.step} step"
+}
