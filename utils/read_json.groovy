9b50ed5e30593306b9c293be5173fd028bfb127a
dwctua <46290157+dwctua@users.noreply.github.com>
2020-05-17 13:59:15 -0400

    Update read_json.groovy

--- utils/read_json.groovy
+++ utils/read_json.groovy
@@ -4,6 +4,7 @@ def call(String filename)  {
   sh """
     pwd
     ls -lart config
+    ls -lart /home/jenkins/workspace/test-shared-lib_master/config/host.json
   """
   def jsonSlurper = new JsonSlurper()
   data = jsonSlurper.parse(new File(filename))
