e515bbfa21c276527288638cf190c479e6f7b19c
dwctua <46290157+dwctua@users.noreply.github.com>
2020-05-17 13:54:32 -0400

    Update read_json.groovy

--- utils/read_json.groovy
+++ utils/read_json.groovy
@@ -1,6 +1,9 @@
 import groovy.json.JsonSlurper
 
 def call(String filename)  {
+  sh """
+    pwd
+  """
   def jsonSlurper = new JsonSlurper()
   data = jsonSlurper.parse(new File(filename))
   println(data)
