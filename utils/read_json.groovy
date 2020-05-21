a349669debe1e2d61e7eccb71a70b81d97edfe4a
dwctua <46290157+dwctua@users.noreply.github.com>
2020-05-20 20:55:28 -0400

    Update read_json.groovy

--- utils/read_json.groovy
+++ utils/read_json.groovy
@@ -3,9 +3,9 @@ import groovy.json.JsonSlurper
 def call(String filename)  {
 
   json = readFile(filename)
-  def jsonSlurper = new JsonSlurper()
-  data = new HashMap<>(jsonSlurper.parseText(json))
-  println(data['test'][0])
+  //def jsonSlurper = new JsonSlurper()
+  //data = new HashMap<>(jsonSlurper.parseText(json))
+  //println(data['test'][0])
   sh """
     pwd
     ls -lart config
